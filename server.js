0a274521d6580bb04599eb27859b6878affe039c
krishramoju2 <futureai.incorporated@gmail.com>
2025-06-23 17:53:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,11 +8,25 @@ const io = new Server(server);
 
 app.use(express.static(__dirname)); // serve index.html and script.js
 
+// Store last 100 messages
+const messageHistory = [];
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // Send message history to the newly connected client
+  socket.emit('message history', messageHistory);
+
+  // When a message is received
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // broadcast to all
+    const message = { text: msg, time: new Date().toLocaleTimeString() };
+
+    // Add to history and limit to 100 messages
+    messageHistory.push(message);
+    if (messageHistory.length > 100) messageHistory.shift();
+
+    // Broadcast the message to everyone
+    io.emit('chat message', message);
   });
 
   socket.on('disconnect', () => {
